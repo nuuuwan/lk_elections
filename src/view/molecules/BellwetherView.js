03937dff8f030ebf76ad8c6d186601d611608f97
nuuuwan <nuuuwan@gmail.com>
2024-04-17 18:22:20 +0530

    BellwetherView uses DataTableView

--- src/view/molecules/BellwetherView.js
+++ src/view/molecules/BellwetherView.js
@@ -1,48 +1,29 @@
-import { AnalysisBellwether } from "../../nonview/core";
-import { Format } from "../../nonview/base";
-import { Header, ElectionLink, SectionBox, PartyLink } from "../atoms";
+import { Party, AnalysisBellwether } from "../../nonview/core";
 
-function renderPercent(l1Error) {
-  let l1ErrorStr = Format.percent(l1Error);
-  let l1ErrorColor = "#ccc";
-  if (l1Error < 0.02) {
-    l1ErrorColor = "#000";
-  } else if (l1Error < 0.1) {
-    l1ErrorColor = "#888";
-  }
-  return (
-    <td className="td-number" style={{ color: l1ErrorColor }}>
-      {l1ErrorStr}
-    </td>
-  );
+import { Header,  SectionBox } from "../atoms";
+import {DataTableView} from "../molecules";
+
+
+function getDataList(elections, ent) {
+  return elections.map(function (election) {
+    const stats = AnalysisBellwether.statsForElection(election, ent);
+    if (!stats) {
+      return null;
+    }
+    const { winningPartyEnt, winningPartyLK, l1Error, isMatch } = stats;
+
+    return {
+      Election: election,
+      Winner: new Party(winningPartyEnt),
+      SriLanka: new Party( winningPartyLK),
+      Match: isMatch,
+      Error: l1Error,
+    };
+  });
+
 }
 
-function BellwetherViewForElection({
-  election,
-  winningPartyEnt,
-  winningPartyLK,
-  l1Error,
-  isMatch,
-}) {
-  return (
-    <tr>
-      <td>
-        <ElectionLink election={election} />
-      </td>
-      <td>
-        <PartyLink partyID={winningPartyEnt} />
-      </td>
-      <td>
-        <PartyLink partyID={winningPartyLK} />
-      </td>
 
-      <td>
-        <PartyLink partyID={winningPartyLK}>{isMatch ? "✓" : " "}</PartyLink>
-      </td>
-      {renderPercent(l1Error)}
-    </tr>
-  );
-}
 
 export default function BellwetherView({ elections, ent }) {
   let n = 0,
@@ -52,52 +33,7 @@ export default function BellwetherView({ elections, ent }) {
   return (
     <SectionBox>
       <Header level={2}>Bellwether Analysis</Header>
-      <table>
-        <thead>
-          <tr>
-            <th>Election</th>
-            <th>{ent.name}</th>
-            <th>Sri Lanka</th>
-            <th></th>
-            <th>Mean Diff.</th>
-          </tr>
-        </thead>
-        <tbody>
-          {elections.map(function (election, iElection) {
-            const stats = AnalysisBellwether.statsForElection(election, ent);
-            if (!stats) {
-              return null;
-            }
-            const { winningPartyEnt, winningPartyLK, l1Error, isMatch } = stats;
-            n += 1;
-            if (isMatch) {
-              nMatch += 1;
-            }
-            errorSum += l1Error;
-
-            return (
-              <BellwetherViewForElection
-                key={"election-" + iElection}
-                election={election}
-                winningPartyEnt={winningPartyEnt}
-                winningPartyLK={winningPartyLK}
-                l1Error={l1Error}
-                isMatch={isMatch}
-              />
-            );
-          })}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan="3"></td>
-
-            <td className="td-number">
-              {nMatch}/{n}
-            </td>
-            {renderPercent(errorSum / n)}
-          </tr>
-        </tfoot>
-      </table>
+      <DataTableView dataList={getDataList(elections, ent)} />  
     </SectionBox>
   );
 }

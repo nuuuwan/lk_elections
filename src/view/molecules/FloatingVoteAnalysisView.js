c401597fc7c072267c0326b4daf34e08ec2c5c51
nuuuwan <nuuuwan@gmail.com>
2024-04-20 11:48:56 +0530

    Use MatrixView in FloatingVoteAnalysisView

--- src/view/molecules/FloatingVoteAnalysisView.js
+++ src/view/molecules/FloatingVoteAnalysisView.js
@@ -1,40 +1,55 @@
-import { Fraction } from "../../nonview/base";
-import { PartyGroup } from "../../nonview/core";
+import { Fraction, SparseMatrix } from "../../nonview/base";
+
 
 import { Header, SectionBox } from "../atoms";
 
-import DataTableView from "./DataTableView";
 
-function getDataList(partyGroups, elections, ents) {
-  return ents.map(function (ent) {
-    let d = {};
+import MatrixView from "./MatrixView";
+
+function getSparseMatrix(partyGroups, elections, ents) {
+  const sparseMatrix = new SparseMatrix();
+
+  for (let ent of ents) {
+  
     let sumBase = 0;
     for (let partyGroup of partyGroups) {
       const { windowBase, electors } = partyGroup.getBaseAnalysisInfo(
         elections,
         ent
       );
-      d[partyGroup.id] = new Fraction(
+      const base = new Fraction(
         Math.round(windowBase * electors, 0),
         electors
       );
+
+      sparseMatrix.push({
+        Region: ent,
+        PartyGroup: partyGroup.id,
+        Base: base,
+      });
+    
       if (windowBase && windowBase > 0) {
         sumBase += windowBase;
       }
     }
-    d = Object.fromEntries(Object.entries(d).sort((a, b) => b[1].p - a[1].p));
+    
 
-    const topPartyGroupID = Object.keys(d)[0];
-    d[topPartyGroupID].color = PartyGroup.fromID(topPartyGroupID).color;
+    const electors = elections.filter((e) => !e.isFuture)[0].getResults(ent.id).summary.electors;
 
-    const electors = Object.values(d)[0].d;
 
-    d["Floating"] = new Fraction(
+    const floating = new Fraction(
       Math.round((1 - sumBase) * electors, 0),
       electors
     );
-    return Object.assign({ Region: ent }, d);
-  });
+    sparseMatrix.push({
+      Region: ent,
+      PartyGroup: "Floating",
+      Base: floating,
+    });
+
+  };
+
+  return sparseMatrix;
 }
 
 export default function FloatingVoteAnalysisView({
@@ -42,11 +57,11 @@ export default function FloatingVoteAnalysisView({
   elections,
   ents,
 }) {
-  const dataList = getDataList(partyGroups, elections, ents);
+  const sparseMatrix = getSparseMatrix(partyGroups, elections, ents);
   return (
     <SectionBox>
       <Header level={2}>Base/Floating Vote Analysis</Header>
-      <DataTableView dataList={dataList} />
+      <MatrixView sparseMatrix={sparseMatrix} xKey="PartyGroup" yKey="Region" zKey="Base" />
     </SectionBox>
   );
 }

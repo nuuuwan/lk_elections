5f5310a4477792b6ecc552c1bdded909914fc31d
nuuuwan <nuuuwan@gmail.com>
2024-04-22 16:45:16 +0530

    Added description to SectionBox (unused)

--- src/view/atoms/SectionBox.js
+++ src/view/atoms/SectionBox.js
@@ -1,11 +1,12 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Header from "./Header";
 
-export default function SectionBox({ children, title }) {
+export default function SectionBox({ children, title, description }) {
+  description = description || "Description TODO";
   return (
     <Box sx={{ marginBottom: 3 }}>
       <Header level={2}>{title}</Header>
-
+      <Typography variant="body1">{description}</Typography>
       {children}
     </Box>
   );

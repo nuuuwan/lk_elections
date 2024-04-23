727bac7664f74d59a6b32c3f1a000f2c4dcbc1cc
nuuuwan <nuuuwan@gmail.com>
2024-04-23 09:56:54 +0530

    lint only

--- src/view/atoms/SectionBox.js
+++ src/view/atoms/SectionBox.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Header from "./Header";
 
 export default function SectionBox({ children, title, description }) {

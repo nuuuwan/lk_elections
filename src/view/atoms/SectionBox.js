be82817191a3265b47da746802e263ef31038235
nuuuwan <nuuuwan@gmail.com>
2024-04-28 17:26:22 +0530

    CleanedupSectionBox

--- src/view/atoms/SectionBox.js
+++ src/view/atoms/SectionBox.js
@@ -2,31 +2,21 @@ import { Box, Typography } from "@mui/material";
 import Header from "./Header";
 import { SmallWindow } from "../../nonview/base";
 
-export default function SectionBox({
-  children,
-  title,
-  description,
-  source,
-  noMaxWidth = false,
-}) {
+export default function SectionBox({ children, title, description, source }) {
   description = description || "Description TODO";
   source = source || "elections.gov.lk";
-  const width = SmallWindow.WIDGET_WIDTH;
 
   return (
     <Box
       sx={{
-        minWidth: width,
+        minWidth: SmallWindow.WIDGET_WIDTH,
       }}
     >
       <Header level={2} id="lk-elections-widget-text-title">
         {title}
       </Header>
       <Box
-        sx={{
-          paddingBottom: 3,
-          paddingTop: 3,
-        }}
+        sx={{ paddingBottom: 3, paddingTop: 3 }}
         id="lk-elections-widget-text-body"
       >
         {description}

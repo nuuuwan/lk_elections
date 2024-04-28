7df9c25492184757410a526d538ffe053e9b9458
nuuuwan <nuuuwan@gmail.com>
2024-04-28 15:45:32 +0530

    Removed unused

--- src/nonview/base/SparseMatrix.js
+++ src/nonview/base/SparseMatrix.js
@@ -11,13 +11,13 @@ export default class SparseMatrix {
     this.dataList.push(data);
   }
 
-  toScalar(x) {
+  __toScalar__(x) {
     const scalar = Scalar.get(x);
     this.scalarToOriginal[scalar] = x;
     return scalar;
   }
 
-  __getOrderedScalarList([xKey, yKey, zKey], [sortYScalar, sortReverse]) {
+  __getOrderedScalarList__([xKey, yKey, zKey], [sortYScalar, sortReverse]) {
     const idx = this.getIdx(yKey, xKey, zKey);
     const orderedScalarList = Object.entries(idx)
       .sort(function ([xScalarA, yScalarToZA], [xScalarB, yScalarToZB]) {
@@ -54,38 +54,18 @@ export default class SparseMatrix {
     );
   }
 
-  getIdxScalar(xKey, yKey, zKey) {
-    return this.dataList.reduce(
-      function (idx, data) {
-        const x = data[xKey];
-        const y = data[yKey];
-        const z = data[zKey];
-
-        const xScalar = this.toScalar(x);
-        const yScalar = this.toScalar(y);
-
-        if (!idx[yScalar]) {
-          idx[yScalar] = {};
-        }
-        idx[yScalar][xScalar] = this.toScalar(z);
-        return idx;
-      }.bind(this),
-      {}
-    );
-  }
-
   getIdxOrdered(
     [xKey, yKey, zKey],
     [sortXScalar, sortXReverse],
     [sortYScalar, sortYReverse]
   ) {
     const idx = this.getIdx(xKey, yKey, zKey);
-    return this.__getOrderedScalarList(
+    return this.__getOrderedScalarList__(
       [xKey, yKey, zKey],
       [sortYScalar, sortYReverse]
     ).reduce(
       function (idxOrdered, xScalar) {
-        return this.__getOrderedScalarList(
+        return this.__getOrderedScalarList__(
           [yKey, xKey, zKey],
           [sortXScalar, sortXReverse]
         ).reduce(function (idxOrdered, yScalar) {

d119496af9844680e99615daaace22dae6ba1686
nuuuwan <nuuuwan@gmail.com>
2024-04-28 11:21:00 +0530

    Removed sinhala and old-left

--- src/nonview/core/PartyGroup/PARTY_GROUP_LIST_TUPLES.js
+++ src/nonview/core/PartyGroup/PARTY_GROUP_LIST_TUPLES.js
@@ -2,14 +2,13 @@ const PARTY_GROUP_LIST_TUPLES = [
   ["Greens", ["UNP", "SJB", "NDF"], "green"],
   ["Blues", ["SLPP", "UPFA", "PA", "SLFP"], "blue"],
   ["Reds", ["JVP", "JJB", "NMPP", "NPP", "DNA"], "red"],
-  ["Old-Left", ["LSSP", "CP", "MEP", "SLMP"], "red"],
+
   [
     "Tamil",
     ["ITAK", "TNA", "EPDP", "ACTC", "AITC", "DPLF", "TMVP", "TULF", "AITM"],
     "orange",
   ],
   ["Muslim", ["SLMC", "ACMC", "MNA", "NUA", "ACMC", "NC"], "teal"],
-  ["Sinhala", ["JHU", "SU", "OPPP"], "#f80"],
 ];
 
 export default PARTY_GROUP_LIST_TUPLES;

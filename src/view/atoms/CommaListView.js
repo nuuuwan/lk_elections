8ce989644cf6df751aab6f677f4176de0a3933c2
nuuuwan <nuuuwan@gmail.com>
2024-04-28 19:42:26 +0530

    Simplified CommaListView

--- src/view/atoms/CommaListView.js
+++ src/view/atoms/CommaListView.js
@@ -1,20 +1,23 @@
 import { Box } from "@mui/material";
 
+function getSuffix(iChild, nChildren) {
+  if (iChild === nChildren - 2) {
+    return " and ";
+  } else if (iChild === nChildren - 1) {
+    return "";
+  }
+  return ", ";
+}
+
 export default function CommaListView({ children }) {
   const nChildren = children.length;
   return (
     <Box component="span">
       {children.map(function (child, iChild) {
-        let suffix = ", ";
-        if (iChild === nChildren - 2) {
-          suffix = " and ";
-        } else if (iChild === nChildren - 1) {
-          suffix = "";
-        }
         return (
           <Box key={"comma-list-view" + iChild} component="span">
             {child}
-            {suffix}
+            {getSuffix(iChild, nChildren)}
           </Box>
         );
       })}

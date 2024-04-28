3919ff91ffe80ae97c7e727eede6410970936e43
nuuuwan <nuuuwan@gmail.com>
2024-04-28 19:45:50 +0530

    split out getColor

--- src/view/atoms/ElectionLink.js
+++ src/view/atoms/ElectionLink.js
@@ -3,6 +3,16 @@ import { LinkContext } from "../atoms";
 
 import { Party } from "../../nonview/core";
 
+function getColor(election) {
+  const results = election.getResults("LK");
+  if (!results) {
+    return null;
+  }
+  const winningPartyID = results.partyToVotes.winningParty;
+  const winningParty = Party.fromID(winningPartyID);
+  return winningParty.color;
+}
+
 export default function ElectionLink({ election }) {
   const context = {
     pageID: "Election",
@@ -10,13 +20,8 @@ export default function ElectionLink({ election }) {
   };
 
   const emoji = election.isFuture ? "⏳" : "";
-  const results = election.getResults("LK");
-  let color = null;
-  if (results) {
-    const winningPartyID = results.partyToVotes.winningParty;
-    const winningParty = Party.fromID(winningPartyID);
-    color = winningParty.color;
-  }
+  const color = getColor(election);
+
   return (
     <LinkContext context={context}>
       <Typography variant="inherit" sx={{ color }} component="span">

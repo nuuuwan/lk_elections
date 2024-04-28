9acfd1db2df9a9b913ba1b7eaa07f38f204c9742
nuuuwan <nuuuwan@gmail.com>
2024-04-28 17:24:33 +0530

    Split GenericStore

--- src/nonview/core/GenericStore.js
+++ src/nonview/core/GenericStore.js
@@ -3,15 +3,16 @@ import { Election, Party, PartyGroup } from "../core";
 import Demographics from "./Demographics/Demographics";
 
 export default class GenericStore {
-  static async get() {
-    // Elections
+  static async getElections() {
     const elections = await Election.listAll();
     const completedElections = Election.filterCompleted(elections);
     const parliamentaryELections = elections.filter(
       (election) => election.electionType === "parliamentary"
     );
+    return { elections, completedElections, parliamentaryELections };
+  }
 
-    // Ents
+  static async getEnts() {
     const countryEnt = await Ent.fromID("LK");
     const edEnts = await Ent.listFromType(EntType.ED);
     const pdEnts = await Ent.listFromType(EntType.PD);
@@ -23,23 +24,26 @@ export default class GenericStore {
       ...pdEnts,
     ]);
 
+    return { countryEnt, edEnts, pdEnts, demographicsIdx };
+  }
+
+  static async get() {
     // Parties
     const partyList = Party.listAll();
 
     // Party Group
     const partyGroupList = PartyGroup.listAll();
 
-    const newState = {
-      elections,
-      completedElections,
-      parliamentaryELections,
-      countryEnt,
-      edEnts,
-      pdEnts,
-      partyList,
-      partyGroupList,
-      demographicsIdx,
-    };
+    const newState = Object.assign(
+      {},
+      await GenericStore.getElections(),
+      await GenericStore.getEnts(),
+      {
+        partyList,
+        partyGroupList,
+      }
+    );
+    console.debug(newState);
     return newState;
   }
 }

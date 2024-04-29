6818459a39f8153459009d0e4a6b7391131df1c9
nuuuwan <nuuuwan@gmail.com>
2024-04-29 14:20:00 +0530

    PartyElectoralSummaryView: Use MatrixView

--- src/view/molecules/PartyElectoralSummaryView.js
+++ src/view/molecules/PartyElectoralSummaryView.js
@@ -1,42 +1,42 @@
 import { ElectionLink, Essay, PartyLink, SectionBox } from "../atoms";
-import { DataTableView } from ".";
-import { Format, Fraction } from "../../nonview/base";
+import { MatrixView } from ".";
+import { Format, Fraction, SparseMatrix } from "../../nonview/base";
 import { Box } from "@mui/material";
 
-function getDataList(party, elections) {
-  return elections
-    .sort()
-    .reverse()
-    .map(function (election) {
-      const resultsForLK = election.getResults("LK");
-      if (!resultsForLK) {
-        return null;
-      }
-      const partyToVotes = resultsForLK.partyToVotes;
-      const votes = partyToVotes.partyToVotes[party.id];
-      if (!votes) {
-        return null;
-      }
-      const pVotes = partyToVotes.partyToPVotes[party.id];
+function getSparseMatrix(party, elections) {
+  return elections.reverse().reduce(function (sparseMatrix, election) {
+    const resultsForLK = election.getResults("LK");
+    if (!resultsForLK) {
+      return sparseMatrix;
+    }
+    const partyToVotes = resultsForLK.partyToVotes;
+    const votes = partyToVotes.partyToVotes[party.id];
+    if (!votes) {
+      return sparseMatrix;
+    }
+    const pVotes = partyToVotes.partyToPVotes[party.id];
+    let position = Object.keys(partyToVotes.partyToVotes).indexOf(party.id) + 1;
+    const fraction = new Fraction(votes, Math.round(votes / pVotes, 0));
 
-      let position =
-        Object.keys(partyToVotes.partyToVotes).indexOf(party.id) + 1;
-
-      const fraction = new Fraction(votes, Math.round(votes / pVotes, 0));
-
-      return {
+    return Object.entries({
+      Position: position,
+      Votes: fraction,
+    }).reduce(function (sparseMatrix, [key, value]) {
+      return sparseMatrix.push({
         Election: election,
-        Position: position,
-        Votes: fraction,
-      };
-    })
-    .filter((x) => x);
+        Key: key,
+        Value: value,
+      });
+    }, sparseMatrix);
+  }, new SparseMatrix());
 }
 
-function getTitleAndDescription(party, elections, dataList) {
+function getTitleAndDescription(party, elections, sparseMatrix) {
   const nAll = elections.length;
-  const n = dataList.length;
-  const best = dataList.sort((a, b) => b.Votes.p - a.Votes.p)[0];
+  const n = sparseMatrix.dataList.length / 2;
+  const best = sparseMatrix.dataList
+    .filter((a) => a.Key === "Votes")
+    .sort((a, b) => b.Value.p - a.Value.p)[0];
   const title = (
     <Box component="span">
       What is
@@ -52,7 +52,7 @@ function getTitleAndDescription(party, elections, dataList) {
       </>
       <>
         Best showing (% wise) was in <ElectionLink election={best.Election} /> ,
-        when it got {Format.percent(best.Votes.p)} of the vote, & came{" "}
+        when it got {Format.percent(best.Value.p)} of the vote, & came{" "}
         {Format.position(best.Position)}.
       </>
     </Essay>
@@ -61,15 +61,20 @@ function getTitleAndDescription(party, elections, dataList) {
 }
 
 export default function PartyElectoralSummaryView({ party, elections }) {
-  const dataList = getDataList(party, elections);
+  const sparseMatrix = getSparseMatrix(party, elections);
   const { title, description } = getTitleAndDescription(
     party,
     elections,
-    dataList
+    sparseMatrix
   );
   return (
     <SectionBox title={title} description={description}>
-      <DataTableView dataList={dataList} />
+      <MatrixView
+        sparseMatrix={sparseMatrix}
+        xKey="Election"
+        yKey="Key"
+        zKey="Value"
+      />
     </SectionBox>
   );
 }
